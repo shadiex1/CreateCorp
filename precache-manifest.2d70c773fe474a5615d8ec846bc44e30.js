5782a32fed3ceda1c62ae8ea667e5b62cfc86bf2
shady ahmed <shadiex@hotmail.com>
2020-05-04 01:01:27 +0200

    Updates

---
+++ precache-manifest.2d70c773fe474a5615d8ec846bc44e30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145e08948cd059860c04f0ef2ca04ef5",
+    "url": "/CreateCorp/index.html"
+  },
+  {
+    "revision": "f9e0ec69495a6c336bee",
+    "url": "/CreateCorp/static/css/main.b56e8b0c.chunk.css"
+  },
+  {
+    "revision": "42938c69f10bb335ea62",
+    "url": "/CreateCorp/static/js/2.7f7dc0eb.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/CreateCorp/static/js/2.7f7dc0eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e0ec69495a6c336bee",
+    "url": "/CreateCorp/static/js/main.edaed44e.chunk.js"
+  },
+  {
+    "revision": "53cf4fbd5f61e5fb0a5d",
+    "url": "/CreateCorp/static/js/runtime-main.70288b22.js"
+  }
+]);
