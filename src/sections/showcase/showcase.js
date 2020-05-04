cdbfe8c3275f16e11d4d9d7b062dd9a58b865157
shady ahmed <shadiex@hotmail.com>
2020-05-04 23:01:29 +0200

    fixed showcase header

--- src/sections/showcase/showcase.js
+++ src/sections/showcase/showcase.js
@@ -26,7 +26,7 @@ class Showcase extends Component {
         </div>
         <div className={styles.showcase}>
              <img className={styles.showcaseImg} src="https://images.unsplash.com/3/doctype-hi-res.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1046&q=80" alt=""/>
-             <p className={styles.showcasetext}>We design and develop experiences that make people's live   <span className={styles.red}>simple</span></p>
+             <p className={styles.showcasetext}>We design and develop experiences that make people's lives   <span className={styles.red}>simple</span></p>
         </div>
         <div className={styles.showcase}>
              <img className={styles.showcaseImg} src="https://images.unsplash.com/photo-1493421419110-74f4e85ba126?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80" alt=""/>
