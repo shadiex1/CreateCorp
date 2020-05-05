1a49b6f41d6f1865e65a0d9946b56395c8ee821b
shady ahmed <shadiex@hotmail.com>
2020-05-05 22:13:38 +0200

    fixed title

--- src/sections/showcase/showcase.js
+++ src/sections/showcase/showcase.js
@@ -21,12 +21,12 @@ class Showcase extends Component {
             }
           }}>
         <div className={styles.showcase}>
-             <img className={styles.showcaseImg} src="http://designcrazzy.com/demo/elitecorp/images/banner.jpg" alt=""/>
-             <p className={styles.showcasetext}>We're an independent design & <span className={styles.red}>development</span> agency.</p>
+              <img className={styles.showcaseImg} src="https://images.unsplash.com/3/doctype-hi-res.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1046&q=80" alt=""/>
+              <p className={styles.showcasetext}>We're an independent design & <span className={styles.red}>development</span> agency.</p>
         </div>
         <div className={styles.showcase}>
-             <img className={styles.showcaseImg} src="https://images.unsplash.com/3/doctype-hi-res.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1046&q=80" alt=""/>
-             <p className={styles.showcasetext}>We design and develop experiences that make people's lives   <span className={styles.red}>simple</span></p>
+              <img className={styles.showcaseImg} src="http://designcrazzy.com/demo/elitecorp/images/banner.jpg" alt=""/>
+              <p className={styles.showcasetext}>We design and develop experiences that make people's lives   <span className={styles.red}>simple</span></p>
         </div>
         <div className={styles.showcase}>
              <img className={styles.showcaseImg} src="https://images.unsplash.com/photo-1493421419110-74f4e85ba126?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80" alt=""/>
